da81a8181227a99d6df802d7af235ba99d6032c0
nicoloso100 <nico.las0315@hotmail.com>
2020-07-31 13:07:15 -0500

    Implemented react redux with hooks and typescript

--- AndroidMovies/src/components/atoms/switch/index.tsx
+++ AndroidMovies/src/components/atoms/switch/index.tsx
@@ -2,26 +2,18 @@ import * as React from 'react';
 import {Switch} from 'react-native-gesture-handler';
 
 interface MySwitchProps {
-  defaultValue?: boolean;
-  onChange?: () => void;
+  value: boolean;
+  onChange: (state: boolean) => void;
 }
 
-const MySwitch: React.FC<MySwitchProps> = ({defaultValue}) => {
-  const [isEnabled, setIsEnabled] = React.useState(false);
-
-  React.useEffect(() => {
-    setIsEnabled(isEnabled);
-  }, [defaultValue]);
-
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
-
+const MySwitch: React.FC<MySwitchProps> = ({value, onChange}) => {
   return (
     <Switch
       trackColor={{false: '#767577', true: '#81b0ff'}}
-      thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+      thumbColor={value ? '#f5dd4b' : '#f4f3f4'}
       ios_backgroundColor="#3e3e3e"
-      onValueChange={toggleSwitch}
-      value={isEnabled}
+      onValueChange={onChange}
+      value={value}
     />
   );
 };
--- AndroidMovies/src/components/molecules/darkModeSwitch/index.tsx
+++ AndroidMovies/src/components/molecules/darkModeSwitch/index.tsx
@@ -3,20 +3,27 @@ import Label from '@components/atoms/label';
 import {DarkModeSwitchContainer} from './styles';
 import MySwitch from '@components/atoms/switch';
 import {useSelector, useDispatch} from 'react-redux';
+import {SET_DARKMODE_ENABLED, SET_DARKMODE_DISABLED} from '@utils/constants';
+import {RootState} from '@reducers/index';
 
 interface DarkModeSwitchProps {}
 
 const DarkModeSwitch: React.FC<DarkModeSwitchProps> = () => {
-  const selector = useSelector((state) => state);
+  const selector = useSelector((state: RootState) => state.darkMode);
   const dispatch = useDispatch();
-  console.log(selector);
 
-  const onSwitchChanged = () => {};
+  const onSwitchChanged = (checked: boolean) => {
+    if (checked) {
+      dispatch({type: SET_DARKMODE_ENABLED});
+    } else {
+      dispatch({type: SET_DARKMODE_DISABLED});
+    }
+  };
 
   return (
     <DarkModeSwitchContainer>
       <Label text="Habilitar darkmode" />
-      <MySwitch onChange={} />
+      <MySwitch value={selector.enabled} onChange={onSwitchChanged} />
     </DarkModeSwitchContainer>
   );
 };
